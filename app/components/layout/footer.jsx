8ac86594e9282137d4b1390c3e268d84c8d58caf
choace0427 <jacobkevin0427@gmail.com>
2024-04-03 15:02:40 -0400

    fix

--- app/components/layout/footer.jsx
+++ app/components/layout/footer.jsx
@@ -2,7 +2,7 @@ import { IconX } from '@tabler/icons-react';
 
 export default function Footer() {
   return (
-    <div className='flex justify-between items-center w-full py-4 md:px-14 px-4 mt-20 border-t-2 border-t-[#9F9FA8]/25'>
+    <div className='flex justify-between items-center w-full py-4 md:px-14 px-4 mt-36 border-t-2 border-t-[#9F9FA8]/25'>
       <div className='text-[#9F9FA8]'>© 2024 Airdrop Farming, All rights reserved.</div>
       <IconX color='#9F9FA8' />
     </div>
