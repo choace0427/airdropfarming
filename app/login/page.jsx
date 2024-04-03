6f378385ba15bd566158c20868b83e97bb561399
choace0427 <jacobkevin0427@gmail.com>
2024-04-03 16:01:02 -0400

    fix login page

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -5,7 +5,7 @@ export default function Login() {
         <p className='text-white font-bold text-[28px] text-center'>Enter the world of automated Airdrop Farming</p>
         <p className='text-white font-medium text-center'>One tool to claim them all.</p>
         <p className='text-[#A19997] font-medium text-center'>We offer a streamlined solution for effortless airdrop farming.</p>
-        <div className='border border-black flex w-fit mt-2 bg-gradient-to-r from-[#1E1E1E] to-[#182343]'>
+        <div className='border border-[#131217] border-b-[3px] border-r-[3px] rounded-lg flex w-fit mt-2 bg-gradient-to-r from-[#131217] to-[#182343]'>
           <div className='bg-[#232a4c] flex flex-col gap-4 px-16 py-30 items-center justify-center m-2 rounded-md w-fit'>
             <img src='./metamask.png' className='w-[120px]' />
             <button className='px-20 py-2 mt-5 border-2 border-[#1a284e] rounded-xl bg-gradient-to-r from-[#161E1E] via-[#1C2A52] to-[#161E1E]'>
